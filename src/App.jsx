54bc8049f5885cd23fe44697d044957ead6d1a82
shivanshi <73399160+shivanshi-s@users.noreply.github.com>
2022-10-01 12:26:25 +0000

    About Page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import LandingPage from "./pages/LandingPage";
+import About from "./pages/About";
 import './App.css'
 
 export default function App() {
   return (
     <div className="App">
-      <LandingPage/>
+      <LandingPage />
+      <About />
     </div>
   )
 }
